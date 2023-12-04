bfaa1ee078caeadd9a7f19b026e0c4c7ebad5167
teddy-jeon <144099472+teddy-the-steady@users.noreply.github.com>
2023-12-04 16:57:53 +0900

    Update train.py

--- train.py
+++ train.py
@@ -16,6 +16,7 @@ import copy
 import logging
 import io
 import json
+import os
 from dataclasses import dataclass, field
 from typing import Optional, Dict, Sequence
 
@@ -238,8 +239,10 @@ def train():
     data_module = make_supervised_data_module(tokenizer=tokenizer, data_args=data_args)
     trainer = Trainer(model=model, tokenizer=tokenizer, args=training_args, **data_module)
     trainer.train()
-    trainer.save_state()
-    # safe_save_model_for_hf_trainer(trainer=trainer, output_dir=training_args.output_dir)
+    # trainer.save_state()
+    checkpoint_dir = os.path.join(training_args.output_dir, "checkpoint-final")
+    trainer.deepspeed.save_checkpoint(checkpoint_dir)
+    safe_save_model_for_hf_trainer(trainer=trainer, output_dir=training_args.output_dir)
     model.push_to_hub('teddy-jeon/alpaca-translated-polyglot-ko-12.8b')
 
 
